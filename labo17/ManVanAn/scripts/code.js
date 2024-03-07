f6c365b05d5c33488d145ea708ce5c3f4038800d
r0990731 <r0990731@student.vives.be>
2024-03-07 15:00:12 +0100

    man van an indexOf()

---
+++ labo17/ManVanAn/scripts/code.js
@@ -0,0 +1,31 @@
+const setup = () => {
+    let knopIndexOf = document.getElementById("indexOf")
+    let knopLastIndexOf = document.getElementById("lastIndexOf")
+    console.log("De man van An geeft geen hand aan ambetante verwanten")
+    knopIndexOf.addEventListener("click", indexOf)
+    knopLastIndexOf.addEventListener("click", lastIndexOf)
+}
+
+const indexOf = ()=>{
+    let tekst = "De man van An geeft geen hand aan ambetante verwanten"
+    tekst.toLowerCase()
+    let index = 0
+    let i = 0
+    let teller = 0
+    let doorgelopen = false
+    while(i<tekst.length && !doorgelopen){
+        if(tekst.indexOf("an",index)!==-1){
+            index = tekst.indexOf("an",index)+1
+            teller ++
+            i++
+        }
+        else{
+            doorgelopen = true
+        }
+    }
+    console.log("Het woord 'an' komt " + teller + " keer voor met indexOf()")
+}
+const lastIndexOf = () =>{
+
+}
+window.addEventListener("load", setup);
