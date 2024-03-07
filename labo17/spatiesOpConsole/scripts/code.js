fd6e8d0bc016119cf50beeed2624b7af2e284be8
r0990731 <r0990731@student.vives.be>
2024-03-07 13:20:40 +0100

    Spaties met functies

--- labo17/spatiesOpConsole/scripts/code.js
+++ labo17/spatiesOpConsole/scripts/code.js
@@ -2,10 +2,19 @@ const setup = () => {
     let knop = document.getElementById("button")
     knop.addEventListener("click", verander)
 }
-const verander =() =>{
-    let input = document.getElementById("tekstvak")
-    let tekst = input.value
-
-    console.log(tekst.split(" "))
+const verander = () =>{
+    let tekstvak = document.getElementById("tekstvak")
+    let tekst = tekstvak.value
+    //console.log(tekst.value)
+    console.log(maakMetSpaties(tekst))
+}
+const maakMetSpaties =(inputText) =>{
+    let result = ""
+    for(let i=0;i<inputText.length;i++){
+        if(inputText.charAt(i)!== " "){
+            result += inputText.charAt(i) + " "
+        }
+    }
+    return result
 }
 window.addEventListener("load", setup);
