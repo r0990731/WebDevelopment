177d0e7f8d73cae945773c83347e7bfc6934f8aa
r0990731 <r0990731@student.vives.be>
2024-03-14 14:28:01 +0100

    opdracht gemeenten

--- labo18/OpdrachtGemeenten/scripts/code.js
+++ labo18/OpdrachtGemeenten/scripts/code.js
@@ -1,8 +1,24 @@
 const setup = () => {
-    let gemeente = window.prompt("Gemeente", "onbekend")
+    let gemeenten = []
+    let stop = false
+    let index = 0
 
-    if(gemeente === "stop"){
+    do{
+        let gemeente = window.prompt("Gemeente", "stop")
+        if(gemeente.toLowerCase() !== "stop"){
+            gemeenten[index] = gemeente
+            index++
+        }
+        else{
+            stop = true
+        }
+    } while(!stop)
 
+    gemeenten.sort()
+    let output = document.getElementById("gemeenten")
+    for(let i = 0; i < gemeenten.length; i++){
+        output.innerHTML += "<option value=" + gemeenten[i] + ">" + gemeenten[i] + "</option>"
     }
+
 }
 window.addEventListener("load", setup);
